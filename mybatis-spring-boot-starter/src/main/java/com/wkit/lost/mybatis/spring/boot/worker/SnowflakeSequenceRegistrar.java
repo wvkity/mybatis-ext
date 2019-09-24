8cfbb316e707e3ace406d40e27cd33e8d3b9518a
mddxhmb <mddxhzh@163.com>
2019-09-24 10:53:39 +0800

    修复存在的bug，优化雪花ID序列bean注册

--- mybatis-spring-boot-starter/src/main/java/com/wkit/lost/mybatis/spring/boot/worker/SnowflakeSequenceRegistrar.java
+++ mybatis-spring-boot-starter/src/main/java/com/wkit/lost/mybatis/spring/boot/worker/SnowflakeSequenceRegistrar.java
@@ -23,6 +23,17 @@ import org.springframework.core.type.AnnotationMetadata;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 注册{@link SnowflakeSequence}实例
+ * <p>
+ *     <ul>
+ *         <li>优先从配置文件获取{@link SequenceAutoConfiguration}实例，
+ *         如果获取到的对象是空，则从Bean容器中根据名称和类型匹配获取配置实例，此时还是空的话直接new一个实例</li>
+ *         <li>注册到Bean容器的实例名称为sequence，同时还给该实例起了一个snowflakeSequence别名</li>
+ *     </ul>
+ * </p>
+ * @author DT
+ */
 @AutoConfigureBefore( { MyBatisAutoConfiguration.class } )
 @AutoConfigureAfter( { SequenceAutoConfiguration.class } )
 class SnowflakeSequenceRegistrar implements BeanFactoryAware, EnvironmentAware, ImportBeanDefinitionRegistrar {
@@ -32,26 +43,38 @@ class SnowflakeSequenceRegistrar implements BeanFactoryAware, EnvironmentAware, 
 
     @Override
     public void registerBeanDefinitions( AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry ) {
-        if ( this.configuration == null ) {
-            if ( registry.containsBeanDefinition( "workerAutoConfiguration" ) ) {
-                this.configuration = ( SequenceAutoConfiguration ) beanFactory.getBean( "workerAutoConfiguration" );
-            } else {
-                this.configuration = new SequenceAutoConfiguration();
-            }
-        }
+        checkConfiguration();
         AnnotationAttributes attributes = AnnotationAttributes.fromMap( importingClassMetadata.getAnnotationAttributes( EnableSnowflakeSequence.class.getName() ) );
         boolean secondEnable = Optional.ofNullable( attributes ).map( attr -> attr.getBoolean( "secondEnable" ) ).orElse( false );
         boolean macEnable = Optional.ofNullable( attributes ).map( attr -> attr.getBoolean( "macEnable" ) ).orElse( false );
-        if ( secondEnable ) {
-            this.configuration.setSecondEnable( secondEnable );
-        }
-        if ( macEnable ) {
-            this.configuration.setMacEnable( macEnable );
-        }
-        registerBean( registry );
+        registerBean( registry, secondEnable, macEnable );
     }
 
-    private void registerBean( BeanDefinitionRegistry registry ) {
+    private void checkConfiguration() {
+        if ( this.configuration == null ) {
+            // 从指定的名称获取bean
+            if ( beanFactory.containsBeanDefinition( "sequenceAutoConfiguration" ) ) {
+                try {
+                    this.configuration = beanFactory.getBean( "sequenceAutoConfiguration", SequenceAutoConfiguration.class );
+                } catch ( Exception e ) {
+                    // ignore
+                }
+            }
+            // 从指定类型获取bean
+            if ( this.configuration == null ) {
+                try {
+                    this.configuration = beanFactory.getBean( SequenceAutoConfiguration.class );
+                } catch ( Exception e ) {
+                    // ignore
+                }
+            }
+            if ( this.configuration == null ) {
+                this.configuration = new SequenceAutoConfiguration();
+            }
+        }
+    }
+
+    private void registerBean( BeanDefinitionRegistry registry, boolean secondEnable, boolean macEnable ) {
         BeanDefinitionBuilder definitionBuilder = BeanDefinitionBuilder.genericBeanDefinition( SnowflakeSequence.class );
         GenericBeanDefinition definition = ( GenericBeanDefinition ) definitionBuilder.getRawBeanDefinition();
         definition.setBeanClass( SnowflakeSequence.class );
@@ -59,14 +82,14 @@ class SnowflakeSequenceRegistrar implements BeanFactoryAware, EnvironmentAware, 
         // 属性
         ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
         // 启用秒级
-        if ( configuration.isSecondEnable() ) {
+        if ( configuration.isSecondEnable() || secondEnable ) {
             argumentValues.addIndexedArgumentValue( 0, TimeUnit.SECONDS );
         } else {
             argumentValues.addIndexedArgumentValue( 0, TimeUnit.MILLISECONDS );
         }
         argumentValues.addIndexedArgumentValue( 1, configuration.getEpochTimestamp() );
-        // 使用mac地址动态获取
-        if ( configuration.isMacEnable() ) {
+        // 启用mac地址动态获取
+        if ( configuration.isMacEnable() || macEnable ) {
             argumentValues.addIndexedArgumentValue( 2, SequenceUtil.getDefaultWorkerId( 5, 5 ) );
             argumentValues.addIndexedArgumentValue( 3, SequenceUtil.getDefaultDataCenterId( 5 ) );
         } else {
@@ -74,7 +97,8 @@ class SnowflakeSequenceRegistrar implements BeanFactoryAware, EnvironmentAware, 
             argumentValues.addIndexedArgumentValue( 3, configuration.getDataCenterId() );
         }
         definition.setConstructorArgumentValues( argumentValues );
-        registry.registerBeanDefinition( "workerSequence", definition );
+        registry.registerBeanDefinition( "sequence", definition );
+        registry.registerAlias( "sequence", "snowflakeSequence" );
     }
 
     @Override
@@ -88,13 +112,7 @@ class SnowflakeSequenceRegistrar implements BeanFactoryAware, EnvironmentAware, 
             Binder binder = Binder.get( environment );
             this.configuration = binder.bind( "sequence", SequenceAutoConfiguration.class ).get();
         } catch ( Exception e ) {
-            try {
-                if ( beanFactory.containsBeanDefinition( "workerAutoConfiguration" ) ) {
-                    this.configuration = ( SequenceAutoConfiguration ) beanFactory.getBean( "workerAutoConfiguration" );
-                }
-            } catch ( Exception e1 ) {
-                // ignore
-            }
+            // ignore
         }
     }
 }
