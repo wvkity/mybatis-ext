df76d176de2aa9957272f73a5f35bf75376c3657
wvkity <mddxhzh@163.com>
2020-04-22 00:37:06 +0800

    - 添加基本条件包装类(仅仅只能添加条件)

---
+++ mybatis-core/src/main/java/com/wkit/lost/mybatis/core/wrapper/criteria/BasicCriteria.java
@@ -0,0 +1,53 @@
+package com.wkit.lost.mybatis.core.wrapper.criteria;
+
+import com.wkit.lost.mybatis.core.segment.SegmentManager;
+
+/**
+ * 基本条件包装类
+ * @param <T> 实体类型
+ * @author wvkity
+ */
+public class BasicCriteria<T> extends AbstractCriteriaWrapper<T> {
+
+    private static final long serialVersionUID = -6095556771822358933L;
+
+    /**
+     * 构造方法
+     * @param entityClass 实体类
+     */
+    public BasicCriteria(Class<T> entityClass) {
+        this(entityClass, null);
+    }
+
+    /**
+     * 构造方法
+     * @param entityClass 实体类
+     * @param alias       别名
+     */
+    public BasicCriteria(Class<T> entityClass, String alias) {
+        this.entityClass = entityClass;
+        this.parameterSequence = master.parameterSequence;
+        this.aliasSequence = master.aliasSequence;
+        this.paramValueMappings = master.paramValueMappings;
+        this.segmentManager = new SegmentManager();
+        this.tableAlias = alias;
+        this.builtinAlias = SYS_SQL_ALIAS_PREFIX + this.aliasSequence.incrementAndGet();
+    }
+
+    @Override
+    protected BasicCriteria<T> newInstance() {
+        BasicCriteria<T> instance = new BasicCriteria<>(this.entityClass, this.tableAlias);
+        copy(instance, this);
+        return instance;
+    }
+
+    /**
+     * 创建条件包装对象
+     * @param entity 实体类
+     * @param <T>    泛型类型
+     * @return 条件包装对象
+     */
+    public static <T> BasicCriteria<T> from(final Class<T> entity) {
+        return new BasicCriteria<>(entity);
+    }
+}
